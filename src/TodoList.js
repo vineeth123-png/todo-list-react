ce0ff3f02b59a9cf220bb55e065e6cdb25cc06b2
vineeth123-png <vineethdvs123@gmail.com>
2021-06-26 23:14:27 +0530

    Added a form to take new todo items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       todos: [{ task: "Walk the Fish" }, { task: "Groom chickens" }]
     };
+    this.create = this.create.bind(this);
+  }
+  create(newTodo) {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    });
   }
   render() {
-    const todos = this.state.todos.map((todo) => <Todo task={todo.task} />);
+    const todos = this.state.todos.map((todo) => (
+      <Todo task={todo.task} createTodo={this.create} />
+    ));
     return (
       <div>
+        <NewTodoForm createTodo={this.create} />
         <h1>TodoList</h1>
         <ul>{todos}</ul>
       </div>
