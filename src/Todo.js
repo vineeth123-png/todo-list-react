0cb69744b8c79b8f3f7b29e446d3ccb3e936e839
vineeth123-png <vineethdvs123@gmail.com>
2021-07-07 17:33:58 +0530

    Added functionality to remove the todos

--- src/Todo.js
+++ src/Todo.js
@@ -1,22 +1,62 @@
 import React, { Component } from "react";
 
 class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.handleRemove = this.handleRemove.bind(this);
-  }
-  handleRemove(evt) {
-    this.props.removeTodo(this.props.id);
-  }
-  render() {
-    return (
-      <div>
-        <li>{this.props.task}</li>
-        <button>Edit</button>
-        <button onClick={this.handleRemove}>X</button>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isEditing: false,
+			task: this.props.task
+		};
+		this.handleRemove = this.handleRemove.bind(this);
+		this.toggleForm = this.toggleForm.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleUpdate = this.handleUpdate.bind(this);
+	}
+	handleRemove(evt) {
+		this.props.removeTodo(this.props.id);
+	}
+	toggleForm() {
+		this.setState({ isEditing: !this.state.isEditing });
+	}
+	handleUpdate(evt) {
+		evt.preventDefault();
+		// take new data and pass it to parent.
+		this.props.updateTodo(this.props.id, this.state.task);
+		//this.toggleForm();
+		this.setState({ isEditing: false });
+	}
+	handleChange(evt) {
+		this.setState({
+			[evt.target.name]: [evt.target.value]
+		});
+	}
+	render() {
+		let result;
+		if (this.state.isEditing) {
+			result = (
+				<div>
+					<form onSubmit={this.handleUpdate}>
+						<input
+							type="text"
+							value={this.state.task}
+							name="task"
+							onChange={this.handleChange}
+						/>
+						<button>Save</button>
+					</form>
+				</div>
+			);
+		} else {
+			result = (
+				<div>
+					<li>{this.props.task}</li>
+					<button onClick={this.toggleForm}>Edit</button>
+					<button onClick={this.handleRemove}>X</button>
+				</div>
+			);
+		}
+		return result;
+	}
 }
 
 export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,43 +2,54 @@ import React, { Component } from "react";
 import Todo from "./Todo";
 import NewTodoForm from "./NewTodoForm";
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //todos: [{ task: "Walk the Fish" }, { task: "Groom chickens" }]
-      todos: []
-    };
-    this.create = this.create.bind(this);
-    this.remove = this.remove.bind(this);
-  }
-  create(newTodo) {
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			//todos: [{ task: "Walk the Fish" }, { task: "Groom chickens" }]
+			todos: []
+		};
+		this.create = this.create.bind(this);
+		this.remove = this.remove.bind(this);
+		this.update = this.update.bind(this);
+	}
+	create(newTodo) {
+		this.setState({
+			todos: [...this.state.todos, newTodo]
+		});
+	}
 
-  remove(id) {
-    this.setState({
-      todos: this.state.todos.filter((todo) => todo.id !== id)
-    });
-  }
-  render() {
-    const todos = this.state.todos.map((todo) => (
-      <Todo
-        task={todo.task}
-        key={todo.id}
-        id={todo.id}
-        removeTodo={this.remove}
-      />
-    ));
-    return (
-      <div>
-        <NewTodoForm createTodo={this.create} />
-        <h1>TodoList</h1>
-        <ul>{todos}</ul>
-      </div>
-    );
-  }
+	remove(id) {
+		this.setState({
+			todos: this.state.todos.filter((todo) => todo.id !== id)
+		});
+	}
+	update(id, updatedTask) {
+		const updatedTodos = this.state.todos.map((todo) => {
+			if (todo.id === id) {
+				return { ...todo, task: updatedTask };
+			}
+			return todo;
+		});
+		this.setState({ todos: updatedTodos });
+	}
+	render() {
+		const todos = this.state.todos.map((todo) => (
+			<Todo
+				task={todo.task}
+				key={todo.id}
+				id={todo.id}
+				updateTodo={this.update}
+				removeTodo={this.remove}
+			/>
+		));
+		return (
+			<div>
+				<NewTodoForm createTodo={this.create} />
+				<h1>TodoList</h1>
+				<ul>{todos}</ul>
+			</div>
+		);
+	}
 }
 
 export default TodoList;
